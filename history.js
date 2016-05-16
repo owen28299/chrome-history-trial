4ae3b8ee0372c37aced1d24ce9ea4c053712764b
Owen <owenyangg@gmail.com>
2016-05-16 11:47:50 -1000

    only sends history on install or clicking update

--- history.js
+++ history.js
@@ -9,44 +9,6 @@ myHistory.addEventListener("click", function(event){
   chrome.tabs.create({url : event.target.href});
 });
 
-var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
-var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
-
-var urls = {};
-
-chrome.history.search({
-  'text' : "",
-  'startTime' : oneWeekAgo,
-  'maxResults' : 100000
-}, function(historyItems){
-
-  historyItems.forEach(function(historyItem){
-    var histUrl = historyItem.url.toLowerCase();
-
-    if(histUrl.indexOf("google") === -1){
-      urls[histUrl] = historyItem;
-
-      chrome.history.getVisits({url : historyItem.url}, function(itemInfo){
-        urls[histUrl].info = itemInfo;
-      });
-    }
-
-  });
-
-  $.ajax({
-    type: "POST",
-    url: "http://127.0.0.1:3000/data",
-    dataType: 'json',
-    data: {
-      urls : JSON.stringify(urls)
-    },
-    success: function(data) {
-      console.log(data);
-    }
-  });
-
-});
-
 
 function getUrl(callback){
   var queryInfo = {
